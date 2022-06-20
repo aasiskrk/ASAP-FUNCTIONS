b0d7fced29223f9ddc0491c755174a216fb7dff4
shishir <shisirsharma22@gmail.com>
2022-06-21 00:14:02 +0545

    added frames in dashboard frame

--- admin dashboard/admindashboard.py
+++ admin dashboard/admindashboard.py
@@ -1,11 +1,12 @@
-import tkinter as tk
-from tkinter import CENTER, N, SOLID, PhotoImage, messagebox
+from tkinter import *
+from tkinter import messagebox
 
     
-root = tk.Tk()
+root = Tk()
+
 root.state('zoomed')
 root.title('ASAP-LOGIN V-1.0.0'.center(220))
-root.iconbitmap('logo.ico')
+# root.iconbitmap('Logo.ico')
 root.configure(background='#DDC9FF')
 maincolor='#DDC9FF'
 
@@ -27,36 +28,36 @@ def signout():
 
 
 #frames
-frame1 = tk.Frame(root,bg='red')
-frame2 = tk.Frame(root)
-frame3 = tk.Frame(root)
-frame4 = tk.Frame(root)
-frame5 = tk.Frame(root)
-btnframe = tk.Frame(root)
+frame1 = Frame(root,bg=maincolor)
+frame2 = Frame(root)
+frame3 = Frame(root)
+frame4 = Frame(root)
+frame5 = Frame(root)
+btnframe = Frame(root)
 btnframe.grid(row=0,column=0,sticky=N)
 
 #frame placement
 for frame in (frame1, frame2, frame3, frame4, frame5):
-    frame.grid(row=0,column=1,sticky='nsew')
+    frame.grid(row=0,column=1,sticky='nsew',pady=10,padx=10)
     
 #dashboard-frames
-frame1_title=  tk.Label(frame1, text='Page 1', font='times 35', bg='red')
-frame1_title.pack(expand=True)
+# frame1_title=  Label(frame1, font='times 35', bg='red')
+# frame1_title.pack(expand=True)
 
 
-frame2_title=  tk.Label(frame2, text='Page 2', font='times 35', bg='yellow')
+frame2_title=  Label(frame2, font='times 35', bg='yellow')
 frame2_title.pack(fill='both', expand=True)
 
 
-frame2_title=  tk.Label(frame3, text='Page 3', font='times 35', bg='purple')
+frame2_title=  Label(frame3, font='times 35', bg='purple')
 frame2_title.pack(fill='both', expand=True)
 
 
-frame2_title=  tk.Label(frame4, text='Page 4', font='times 35', bg='blue')
+frame2_title=  Label(frame4, font='times 35', bg='blue')
 frame2_title.pack(fill='both', expand=True)
 
 
-frame2_title=  tk.Label(frame5, text='Page 5', font='times 35', bg='pink')
+frame2_title=  Label(frame5, font='times 35', bg='pink')
 frame2_title.pack(fill='both', expand=True)
 
 
@@ -64,26 +65,57 @@ frame2_title.pack(fill='both', expand=True)
 show_frame(frame1)
 
 #buttons
-frame1_btn = tk.Button(btnframe,activebackground=maincolor,border=0,bg=maincolor, text='Dashboard',compound=CENTER,command=lambda:show_frame(frame1))
+frame1_btn = Button(btnframe,activebackground=maincolor,width=30,border=0,bg=maincolor, text='Dashboard',compound=CENTER,command=lambda:show_frame(frame1))
 frame1_btn.pack(fill='x', ipady=15)
 
 
-frame2_btn = tk.Button(btnframe,width=28,activebackground=maincolor,border=0,bg=maincolor, text='Products',command=lambda:show_frame(frame2))
+frame2_btn = Button(btnframe,activebackground=maincolor,border=0,bg=maincolor, text='Products',command=lambda:show_frame(frame2))
 frame2_btn.pack(fill='x', ipady=15)
 
-frame3_btn = tk.Button(btnframe,activebackground=maincolor,border=0,bg=maincolor, text='Bills',command=lambda:show_frame(frame3))
+frame3_btn = Button(btnframe,activebackground=maincolor,border=0,bg=maincolor, text='Bills',command=lambda:show_frame(frame3))
 frame3_btn.pack(fill='x', ipady=15)
 
-frame4_btn = tk.Button(btnframe,activebackground=maincolor,border=0,bg=maincolor, text='Users',command=lambda:show_frame(frame4))
+frame4_btn = Button(btnframe,activebackground=maincolor,border=0,bg=maincolor, text='Users',command=lambda:show_frame(frame4))
 frame4_btn.pack(fill='x', ipady=15)
 
-frame5_btn = tk.Button(btnframe,activebackground=maincolor,border=0,bg=maincolor, text='Settings',command=lambda:show_frame(frame5))
+frame5_btn = Button(btnframe,activebackground=maincolor,border=0,bg=maincolor, text='Settings',command=lambda:show_frame(frame5))
 frame5_btn.pack(fill='x', ipady=15)
 
-frame6_btn = tk.Button(btnframe,activebackground=maincolor,border=0,bg=maincolor, text='Signout',command=signout)
+frame6_btn = Button(btnframe,activebackground=maincolor,border=0,bg=maincolor, text='Signout',command=signout)
 frame6_btn.pack(fill='x', ipady=15)
 
-#plot
+#frame1
+fm1=Frame(frame1,width=800,height=200)
+fm1.grid(row=0,column=0)
+
+fm2=Frame(frame1,width=350,height=200)
+fm2.grid(row=0,column=1,padx=(10,0))
+
+fm3=Frame(frame1,width=1159,height=260)
+fm3.grid(row=1,column=0,columnspan=2,pady=10)
+
+fm4=Frame(frame1,width=800,height=200)
+fm4.grid(row=2,column=0)
+
+fm5=Frame(frame1,width=350,height=200)
+fm5.grid(row=2,column=1,padx=(10,0))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 root.mainloop()
